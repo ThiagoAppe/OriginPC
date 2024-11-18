09885c964b9fc330fdfaccd0fdf6379c7d59e17f
ThiagoAppe <thiagoapiriz@gmail.com>
2024-11-18 13:43:45 -0300

    Creacion del Footer y se añadio al App.jsx del proyecto

--- React/OriginPc/src/App.jsx
+++ React/OriginPc/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/inicio';
 import Contact from './pages/contacto';
 import { NavbarWithSolidBackground } from './components/navbar';
+import { Footer } from './components/footer';
+
 
 function App() {
   return (
@@ -12,6 +14,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+
+      <Footer/>
+
     </Router>
   );
 }
---
+++ React/OriginPc/src/components/footer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Typography } from '@material-tailwind/react';
+import { FaGithub } from 'react-icons/fa'; // Importa el ícono de GitHub
+
+export function Footer() {
+  return (
+    <div className="bg-slate-700">
+      <footer className="px-4 py-4 lg:px-8 lg:py-4 bg-black text-white">
+        <div className="flex justify-center gap-8">
+          {/* Información de las personas */}
+          {['ThiagoAppe', 'juampixltzz', 'Franconext'].map((name, index) => (
+            <div key={index} className="flex flex-col items-center">
+              <Typography as="p" variant="small" color="blue-gray" className="font-normal">
+                {name}
+              </Typography>
+              <Link
+                to={`https://github.com/${name.toLowerCase()}`}
+                target="_blank"
+                className="text-gray-400 hover:text-white"
+              >
+                <FaGithub className="h-6 w-6" />
+              </Link>
+            </div>
+          ))}
+        </div>
+
+        <div className="mt-4 text-center">
+          <Typography as="p" variant="small" color="blue-gray" className="font-normal">
+            &copy; {new Date().getFullYear()} - Instituto Superior Nuestra Señora Del Buen Viaje
+          </Typography>
+        </div>
+      </footer>
+    </div>
+  );
+}
